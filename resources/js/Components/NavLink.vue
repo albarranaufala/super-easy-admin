50677418d41c6964a4fb3be3d6be78ae7946cb1f
Albarra Naufala <albarraerdanto@gmail.com>
2023-09-15 00:56:58 +0700

    feat: installed laravel, breeze with inertia, and modified AuthenticatedLayout

---
+++ resources/js/Components/NavLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps<{
+    href: string;
+    active?: boolean;
+}>();
+
+const classes = computed(() =>
+    props.active
+        ? "inline-flex items-center p-4 rounded-lg bg-gray-100 text-indigo-500 font-medium leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
+        : "inline-flex items-center p-4 rounded-lg font-medium leading-5 text-gray-500 hover:bg-gray-100 hover:text-indigo-500 focus:outline-none focus:text-gray-700 transition duration-150 ease-in-out"
+);
+</script>
+
+<template>
+    <Link :href="href" :class="classes">
+        <slot />
+    </Link>
+</template>
---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,240 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import { Link } from "@inertiajs/vue3";
+
+const showingSideNav = ref(false);
+</script>
+
+<template>
+    <div>
+        <div class="flex min-h-screen bg-gray-100">
+            <aside
+                class="w-full h-screen max-w-[300px] overflow-auto bg-white border-r sm:block sm:sticky sm:top-0"
+                :class="{
+                    'fixed inset-y-0 left-0 z-20': showingSideNav,
+                    hidden: !showingSideNav,
+                }"
+            >
+                <nav class="p-6">
+                    <div class="flex justify-center">
+                        <Link :href="route('dashboard')">
+                            <ApplicationLogo />
+                        </Link>
+                    </div>
+                    <NavLink
+                        :href="route('dashboard')"
+                        :active="route().current('dashboard')"
+                        class="w-full mt-6"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mr-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                            />
+                        </svg>
+                        Dashboard
+                    </NavLink>
+                    <NavLink
+                        :href="route('dashboard')"
+                        :active="false"
+                        class="w-full mt-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mr-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                            />
+                        </svg>
+
+                        Users
+                    </NavLink>
+                    <NavLink
+                        :href="route('dashboard')"
+                        :active="false"
+                        class="w-full mt-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mr-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                            />
+                        </svg>
+
+                        Roles
+                    </NavLink>
+                    <NavLink
+                        :href="route('dashboard')"
+                        :active="false"
+                        class="w-full mt-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mr-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+                            />
+                        </svg>
+
+                        Data
+                    </NavLink>
+                </nav>
+            </aside>
+            <div class="flex-1">
+                <nav class="z-10 bg-white border-b">
+                    <!-- Primary Navigation Menu -->
+                    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                        <div class="flex justify-between h-16">
+                            <!-- Left -->
+                            <div class="flex items-center">
+                                <!-- Hamburger -->
+                                <div class="flex items-center sm:hidden">
+                                    <button
+                                        @click="
+                                            showingSideNav = !showingSideNav
+                                        "
+                                        class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+                                    >
+                                        <svg
+                                            class="w-6 h-6"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                :class="{
+                                                    hidden: showingSideNav,
+                                                    'inline-flex':
+                                                        !showingSideNav,
+                                                }"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M4 6h16M4 12h16M4 18h16"
+                                            />
+                                            <path
+                                                :class="{
+                                                    hidden: !showingSideNav,
+                                                    'inline-flex':
+                                                        showingSideNav,
+                                                }"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+
+                            <!-- Right -->
+                            <div class="flex items-center ml-6">
+                                <!-- Settings Dropdown -->
+                                <div class="relative">
+                                    <Dropdown align="right" width="48">
+                                        <template #trigger>
+                                            <span
+                                                class="inline-flex rounded-md"
+                                            >
+                                                <button
+                                                    type="button"
+                                                    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none"
+                                                >
+                                                    {{
+                                                        $page.props.auth.user
+                                                            .name
+                                                    }}
+
+                                                    <svg
+                                                        class="ml-2 -mr-0.5 h-4 w-4"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20"
+                                                        fill="currentColor"
+                                                    >
+                                                        <path
+                                                            fill-rule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clip-rule="evenodd"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </template>
+
+                                        <template #content>
+                                            <DropdownLink
+                                                :href="route('profile.edit')"
+                                            >
+                                                Profile
+                                            </DropdownLink>
+                                            <DropdownLink
+                                                :href="route('logout')"
+                                                method="post"
+                                                as="button"
+                                            >
+                                                Log Out
+                                            </DropdownLink>
+                                        </template>
+                                    </Dropdown>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+                <button
+                    v-if="showingSideNav"
+                    class="fixed inset-0 bg-black/50"
+                    @click="showingSideNav = false"
+                ></button>
+
+                <!-- Page Heading -->
+                <header class="bg-white border-b" v-if="$slots.header">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                        <slot name="header" />
+                    </div>
+                </header>
+
+                <!-- Page Content -->
+                <main>
+                    <slot />
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
