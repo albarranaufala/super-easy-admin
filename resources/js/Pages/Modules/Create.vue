95389e0723426f5ebebd005c9907818e04ab9422
Albarra Naufala <albarraerdanto@gmail.com>
2023-10-12 21:44:29 +0700

    feat: sliced new input attribute for select

--- resources/js/Pages/Modules/Create.vue
+++ resources/js/Pages/Modules/Create.vue
@@ -16,26 +16,46 @@ import { SelectOption } from "@/types";
 import DangerButton from "@/Components/Button/DangerButton.vue";
 import { getError } from "@/helper";
 
-const form = useForm<{
+interface Attribute {
+    type: string;
     name: string;
-    attributes: Array<{
-        type: string;
+    options?: Array<{
         name: string;
     }>;
+}
+
+const defaultOptions = [
+    {
+        name: "Option 1",
+    },
+    {
+        name: "Option 2",
+    },
+    {
+        name: "Option 3",
+    },
+];
+
+const form = useForm<{
+    name: string;
+    attributes: Array<Attribute>;
 }>({
     name: "",
     attributes: [
         {
             type: "text",
             name: "Attribute 1",
+            options: defaultOptions,
         },
         {
             type: "text",
             name: "Attribute 2",
+            options: defaultOptions,
         },
         {
             type: "text",
             name: "Attribute 3",
+            options: defaultOptions,
         },
     ],
 });
@@ -49,17 +69,32 @@ const attributeTypes: Array<SelectOption> = [
         value: "switch",
         label: "Switch",
     },
+    {
+        value: "select",
+        label: "Select",
+    },
 ];
 
 const addNewAttribute = () => {
     form.attributes.push({
         type: "text",
         name: `Attribute ${form.attributes.length + 1}`,
+        options: defaultOptions,
     });
 };
 
 const deleteAttribute = (index: number) => {
     form.attributes.splice(index, 1);
+};
+
+const addOption = (attribute: Attribute) => {
+    attribute.options?.push({
+        name: `Option ${attribute.options.length + 1}`,
+    });
+};
+
+const deleteOption = (attribute: Attribute, optionIndex: number) => {
+    attribute.options?.splice(optionIndex, 1);
 };
 
 const submit = () => {
@@ -156,6 +191,71 @@ const submit = () => {
                                         )
                                     "
                                 />
+                                <div v-if="attribute.type === 'select'">
+                                    <div
+                                        v-for="(
+                                            option, optionIndex
+                                        ) in attribute.options"
+                                        class="flex mt-6"
+                                    >
+                                        <TextInput
+                                            id="name"
+                                            v-model="option.name"
+                                            placeholder="Option 1"
+                                            class="w-full"
+                                        />
+                                        <DangerButton
+                                            class="ml-6"
+                                            type="button"
+                                            :disabled="
+                                                attribute.options?.length === 1
+                                            "
+                                            @click="
+                                                deleteOption(
+                                                    attribute,
+                                                    optionIndex
+                                                )
+                                            "
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke-width="1.5"
+                                                stroke="currentColor"
+                                                class="w-6 h-6"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                />
+                                            </svg>
+                                        </DangerButton>
+                                    </div>
+                                    <div class="flex justify-end">
+                                        <PrimaryButton
+                                            type="button"
+                                            class="mt-6"
+                                            @click="addOption(attribute)"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke-width="1.5"
+                                                stroke="currentColor"
+                                                class="w-6 h-6"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    d="M12 4.5v15m7.5-7.5h-15"
+                                                />
+                                            </svg>
+                                        </PrimaryButton>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div>
