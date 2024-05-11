74be0a467f670f723327fea942bd13ab144496e7
Albarra Naufala <albarraerdanto@gmail.com>
2024-05-11 22:18:00 +0700

    feat: turn icosn into separated components

--- resources/js/Pages/Modules/Create.vue
+++ resources/js/Pages/Modules/Create.vue
@@ -12,8 +12,10 @@ import PrimaryButton from "@/Components/Button/PrimaryButton.vue";
 import SecondaryButton from "@/Components/Button/SecondaryButton.vue";
 import InputError from "@/Components/Form/InputError.vue";
 import SelectInput from "@/Components/Form/SelectInput.vue";
-import { SelectOption } from "@/types";
 import DangerButton from "@/Components/Button/DangerButton.vue";
+import IconPlus from "@/Components/Icon/IconPlus.vue";
+import IconTrash from "@/Components/Icon/IconTrash.vue";
+import { SelectOption } from "@/types";
 import { getError } from "@/helper";
 
 interface Attribute {
@@ -217,20 +219,7 @@ const submit = () => {
                                                 )
                                             "
                                         >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                stroke-width="1.5"
-                                                stroke="currentColor"
-                                                class="w-6 h-6"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                                />
-                                            </svg>
+                                            <IconTrash class="w-6 h-6" />
                                         </DangerButton>
                                     </div>
                                     <div class="flex justify-end">
@@ -239,20 +228,7 @@ const submit = () => {
                                             class="mt-6"
                                             @click="addOption(attribute)"
                                         >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                stroke-width="1.5"
-                                                stroke="currentColor"
-                                                class="w-6 h-6"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    d="M12 4.5v15m7.5-7.5h-15"
-                                                />
-                                            </svg>
+                                            <IconPlus class="w-6 h-6" />
                                         </PrimaryButton>
                                     </div>
                                 </div>
@@ -265,20 +241,7 @@ const submit = () => {
                                 :disabled="form.attributes.length === 1"
                                 @click="deleteAttribute(index)"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="w-6 h-6"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                    />
-                                </svg>
+                                <IconTrash class="w-6 h-6" />
                             </DangerButton>
                         </div>
                     </div>
@@ -288,20 +251,7 @@ const submit = () => {
                             class="mt-6"
                             @click="addNewAttribute"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M12 4.5v15m7.5-7.5h-15"
-                                />
-                            </svg>
+                            <IconPlus class="w-6 h-6" />
                         </PrimaryButton>
                     </div>
                 </CardBody>
